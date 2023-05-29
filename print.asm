2b7eaa65c70e48cd63774d8bc67572d8dc3de61e
VENOM-InstantDeath <mohathepro111@gmail.com>
2023-05-29 14:36:47 -0300

    Added newline option

--- print.asm
+++ print.asm
@@ -1,11 +1,20 @@
+section .data
+	NEWLINE db 0xA
 section .text
-_print:
+_print: 			;; print a null-terminated string (rsi,r10)
 	mov rax,0
 	call _loop
 	mov rdx,rax
 	mov rax,1
 	mov rdi,1
 	sub rsi,rdx
+	syscall
+	cmp r10,1
+	jne _ret
+	mov rax,1
+	mov rdi,1
+	mov rsi,NEWLINE
+	mov rdx,1
 	syscall
 	ret
 _loop:
